c7f8dc51874e9702fa519de5126b608c6e309937
⟶⚘Nı̄̈͜ı̄̈ībbʌ Oīı̄̈͜ı̄̈īx𝆺꯭𝅥⎯ <abdullahrx96@gmail.com>
2026-08-05 14:06:32 +0600

    Update antijoin.js

--- modules/events/antijoin.js
+++ modules/events/antijoin.js
@@ -1,25 +1,35 @@
 module.exports.config = {
- name: "antijoin",
- eventType: ["log:subscribe"],
- version: "1.0.0",
- credits: "𝐫𝐗,
- description: "Welcome new members to the group"
+	name: "antijoin",
+	eventType: ["log:subscribe"],
+	version: "1.0.0",
+	credits: "𝐫𝐗",
+	description: "Welcome new members to the group"
 };
 
 module.exports.run = async function ({ event, api, Threads, Users }) {
- 	let data = (await Threads.getData(event.threadID)).data
- 	if (data.newMember == false) return;
- 	if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) return
-    else if(data.newMember == true) {
-    var memJoin = event.logMessageData.addedParticipants.map(info => info.userFbId)
-			for (let idUser of memJoin) {
-					await new Promise(resolve => setTimeout(resolve, 1000));
-					api.removeUserFromGroup(idUser, event.threadID, async function (err) {
-                        if (err) return data["newMember"] = false;
-                            await Threads.setData(event.threadID, { data });
-                              global.data.threadData.set(event.threadID, data);
-                    })
+	let data = (await Threads.getData(event.threadID)).data;
+	if (data.newMember == false) return;
+	if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) return;
+	else if (data.newMember == true) {
+		var memJoin = event.logMessageData.addedParticipants.map(info => info.userFbId);
+		for (let idUser of memJoin) {
+			await new Promise(resolve => setTimeout(resolve, 1000));
+			api.removeUserFromGroup(idUser, event.threadID, async function (err) {
+				if (err) return data["newMember"] = false;
+				await Threads.setData(event.threadID, { data });
+				global.data.threadData.set(event.threadID, data);
+			});
+		}
+
+		return api.sendMessage(
+			`» Your team now has Anti Join mode turned on, please turn it off before adding a new 👻 member`,
+			event.threadID,
+			(err, info) => {
+				if (err) return;
+				setTimeout(() => {
+					api.unsendMessage(info.messageID);
+				}, 3000);
 			}
- 	return api.sendMessage(`» Your team now has Anti Join mode turned on, please turn it off before adding a new 👻 member`, event.threadID);
- }
-}
+		);
+	}
+};
